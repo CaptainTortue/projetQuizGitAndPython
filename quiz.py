ddcd84019b19cd40e1eb5f9e9e247f6226f2ccf2
Tony Dary <tony.dary10@gmail.com>
2024-10-09 12:10:30 +0200

    Ajout de la fonction qui choisi aléatoirement une parti du JSON et rajout de 10 questions supplémentaires

--- quiz.py
+++ quiz.py
@@ -1,5 +1,20 @@
+import json
+import random
+
+with open('quizz_questions.json', encoding='utf-8') as questions_file:
+    listQuestions = json.load(questions_file)
+
+def randomQuestion():
+    idQuestion = random.randint(1, len(listQuestions))
+    if listQuestions[idQuestion] in listQuestions:
+        return listQuestions[idQuestion]
+    else:
+        return "Erreur de choix de la question"
+
 def main():
     print("Bienvenue dans le jeu de quiz!")
+    question = randomQuestion()
+    print(question)
 
 
 if __name__ == "__main__":
