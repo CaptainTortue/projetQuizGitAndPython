b762d93dc58e59873fdcc9a1425567ea5ffad40a
CaptainTortue <tristan.martinez@sfr.fr>
2024-10-10 12:16:10 +0200

    interface questions dynamique (besoin de changer quizz_questions.json

--- quiz.py
+++ quiz.py
@@ -4,19 +4,7 @@ import pygame
 import json
 import random
 
-with open('quizz_questions.json', encoding='utf-8') as questions_file:
-    listQuestions = json.load(questions_file)
-
-def randomQuestion():
-    idQuestion = random.randint(1, len(listQuestions))
-    if listQuestions[idQuestion] in listQuestions:
-        return listQuestions[idQuestion]
-    else:
-        return "Erreur de choix de la question"
-
 print("Bienvenue dans le jeu de quiz!")
-question = randomQuestion()
-print(question)
 
 # pygame setup
 pygame.init()
@@ -29,42 +17,15 @@ running = True
 white = (255, 255, 255)
 green = (0, 255, 0)
 blue = (0, 0, 128)
+red = (255, 0, 0)
 
-# assigning values to X and Y variable
-X = 1280
-Y = 720
-
-listQuestions = [
-    {
-        "id": 1,
-        "categorie": "Science",
-        "question": "Quelle planète est la plus proche du Soleil ?",
-        "options": [
-            "a) Mars",
-            "b) Vénus",
-            "c) Mercure",
-            "d) Jupiter"
-        ],
-        "reponse": "c) Mercure"
-    },
-    {
-        "id": 2,
-        "categorie": "Animaux",
-        "question": "Quel est le plus grand mammifère terrestre ?",
-        "options": [
-            "a) Girafe",
-            "b) Eléphant",
-            "c) Hippopotame",
-            "d) Rhinocéros"
-        ],
-        "reponse": "b) Eléphant"
-    }
-]
+# assigning values to screenWidth and screenHeight variable
+screenWidth = 1280
+screenHeight = 720
 
 # create the display surface object
-# of specific dimension..e(X, Y).
-screen = pygame.display.set_mode((X, Y))
-
+# of specific dimension..e(screenWidth, screenHeight).
+screen = pygame.display.set_mode((screenWidth, screenHeight))
 
 # set the pygame window name
 pygame.display.set_caption('Show Text')
@@ -75,62 +36,107 @@ pygame.display.set_caption('Show Text')
 # 2nd parameter is size of the font
 font = pygame.font.Font('freesansbold.ttf', 32)
 
-# create a text surface object for the question
-text = font.render(listQuestions[0]["question"], True, green, blue)
+with open('quizz_questions.json', encoding='utf-8') as questions_file:
+    listQuestions = json.load(questions_file)
 
-# create a rectangular object for the
-# text surface object
-textRect = text.get_rect()
+def randomQuestion():
+    idQuestion = random.randint(1, len(listQuestions)-1)
+    if listQuestions[idQuestion] in listQuestions:
+        return listQuestions[idQuestion]
+    else:
+        return "Erreur de choix de la question"
 
-# create a rectangular object for each option possible
-options = []
-optionsRect = []
-for i in range(len(listQuestions[0]["options"])):
-    option = font.render(listQuestions[0]["options"][i], True, white, green)
-    #optionRect = option.get_rect()
-    #print(Y // 6 + (i // 2)+1 * 50)
-    #optionRect.center = ((X//4 if i%2 == 0 else (X//4)*3), Y // 6 + ((i // 2)+1) * Y // 4)
-    optionRect= pygame.Rect((X//8 if i%2 == 0 else (X//6)*4), Y // 6 + ((i // 2)+1) * Y // 4, X // 4, Y // 6)
-    options.append(option)
-    optionsRect.append(optionRect)
+def refreshQuestion():
+    question = randomQuestion()
+    text = font.render(question["question"], True, green, blue)
+    textRect = text.get_rect()
+    textRect.center = (screenWidth // 2, screenHeight // 6)
+    options = []
+    optionsRect = []
+    for i in range(len(question["options"])):
+        option = font.render(question["options"][i], True, white, green)
+        optionRect = pygame.Rect((screenWidth//8 if i%2 == 0 else (screenWidth//6)*4), screenHeight // 6 + ((i // 2)+1) * screenHeight // 4, screenWidth // 4, screenHeight // 6)
+        options.append(option)
+        optionsRect.append(optionRect)
+    responses = []
+    responsesRect = []
+    for i in range(len(question["options"])):
+        response = font.render(question["options"][i], True, green, blue)
+        responseRect = pygame.Rect(screenWidth // 2, screenHeight // 6 + (i + 1) * 50, screenWidth // 4, screenHeight // 6)
+        responses.append(response)
+        responsesRect.append(responseRect)
+    return question, text, textRect, options, optionsRect, responses, responsesRect
 
-# create a rectangular object for the response
-responses = []
-responsesRect = []
-for i in range(len(listQuestions[0]["options"])):
-    response = font.render(listQuestions[0]["options"][i], True, green, blue)
-    # create rect object for the question, with big size
-    responseRect = pygame.Rect(X // 2, Y // 6 + (i + 1) * 50, X // 4, Y // 6)
-    responses.append(response)
-    responsesRect.append(responseRect)
+question, text, textRect, options, optionsRect, response, responseRect = refreshQuestion()
 
-# set the center of the rectangular object.
-textRect.center = (X // 2, Y // 6)
+def displayRect(rect, color):
+    pygame.draw.rect(screen, color, rect)
+    # set the correctRect bigger
+    rect.inflate_ip(10, 10)
+    # refresh center of the rect
+    rect.center = (screenWidth // 2, screenHeight // 2)
+    if rect.width >= screenWidth and rect.height >= screenHeight:
+        rect.width = screenWidth // 2
+        rect.height = screenHeight // 2
+        return False
+    return True
+
+
+score = 0
+displayCorrectAnimation = False
+displayIncorrectAnimation = False
+
+# create green rect for correct answer animation
+correctRect = pygame.Rect(screenWidth // 4, screenHeight // 6, screenWidth // 2, screenHeight // 6)
+
+# create red rect for incorrect answer animation
+incorrectRect = pygame.Rect(screenWidth // 4, screenHeight // 6, screenWidth // 2, screenHeight // 6)
 
 while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
     # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    # pygame.QUIT event means the user clicked screenWidth to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # check if the mouse click was within the bounds of the option
+            for i in range(len(optionsRect)):
+                if optionsRect[i].collidepoint(event.pos):
+                    # refresh the question
+                    question, text, textRect, options, optionsRect, responses, responsesRect = refreshQuestion()
+                    # check if the option clicked is the correct answer
+                    if listQuestions[0]["options"][i] == listQuestions[0]["reponse"]:
+                        print("Correct!")
+                        score += 1
+                        displayCorrectAnimation = True
+                    else:
+                        print("Incorrect!")
+                        displayIncorrectAnimation = True
+                    # display the correct answer
+                    #for j in range(len(responses)):
+                    #    screen.blit(responses[j], responsesRect[j])
 
+    if (displayCorrectAnimation):
+        displayCorrectAnimation = displayRect(correctRect, green)
+    elif (displayIncorrectAnimation):
+        displayIncorrectAnimation = displayRect(incorrectRect, red)
+    else:
+        # copying the text surface object
+        # to the display question
+        screen.blit(text, textRect)
 
-    # copying the text surface object
-    # to the display question
-    screen.blit(text, textRect)
-
-    # copying the text surface object
-    # to the display options
-    for i in range(len(options)):
-        # add text to the screen and rect distinctly
-        # display rect for each option
-        pygame.draw.rect(screen, green, optionsRect[i])
-        # draw text
-        screen.blit(options[i], (optionsRect[i].x + optionsRect[i].w // 2 - options[i].get_rect().w // 2, optionsRect[i].y + optionsRect[i].h // 2 - options[i].get_rect().h // 2))
-    # RENDER YOUR GAME HERE
+        # copying the text surface object
+        # to the display options
+        for i in range(len(options)):
+            # add text to the screen and rect distinctly
+            # display rect for each option
+            pygame.draw.rect(screen, green, optionsRect[i])
+            # draw text
+            screen.blit(options[i], (optionsRect[i].x + optionsRect[i].w // 2 - options[i].get_rect().w // 2, optionsRect[i].y + optionsRect[i].h // 2 - options[i].get_rect().h // 2))
+        # RENDER YOUR GAME HERE
 
     # flip() the display to put your work on screen
     pygame.display.flip()
