225b04fd672ac1d300e4f732329c8578ee36e2f0
merbouh aymane <aymane.merbouh@uphf.fr>
2024-10-16 11:42:41 +0200

    second commit, better json management

--- ImportDataJSON.py
+++ ImportDataJSON.py
@@ -1,17 +1,46 @@
-import json
-
-def importJSON(categories,inputs) :
-    JSON_DATA= '{}'
+"""import json
+import os
+def importJSON(categories, inputs):
+    JSON_DATA = '{}'
     data = json.loads(JSON_DATA)
-    inputs[10]=1; #in case no difficulty is selected
-    for x in range (len(categories)):
-        data[categories[x]]=inputs[x]
-    # points multiplier depending on the time required
-    score = inputs[10]*5*(30-int(inputs[1]))
+
+    for x in range(len(inputs)):
+        data[categories[x]] = inputs[x]
+        inputs.append(x)
+
+    with open('finalscore_data.json', 'r') as outfile:
+        data1 = json.load(outfile)
+
+    data1.append(data)
 
     with open('finalscore_data.json', 'w') as outfile:
-        json.dump(data, outfile)
+        json.dump(data1, outfile, indent=4)
 
+importJSON(["timer", "name"], ["10", "aeaeaea"])"""
+import json
+import os
 
-#points multiplier depending on the time required
-categories
+def importJSON(categories, inputs):
+    data = {}
+
+    for x in range(len(inputs)):
+        data[categories[x]] = inputs[x]
+
+    if not os.path.exists('finalscore_data.json'):
+        with open('finalscore_data.json', 'w') as outfile:
+            json.dump([], outfile)
+
+    with open('finalscore_data.json', 'r') as outfile:
+        try:
+            data1 = json.load(outfile)
+            if not isinstance(data1, list):
+                data1 = [data1]
+        except json.JSONDecodeError:
+            data1 = []
+
+    data1.append(data)
+
+    with open('finalscore_data.json', 'w') as outfile:
+        json.dump(data1, outfile, indent=4)
+
+importJSON(["timer", "name", "score"], ["10", "uwu","69"])
